6a76fb19941b9b8a9caf1bb1d5d4784460889686
Valerie Doan <valeriephuongdoan@gmail.com>
2025-05-29 19:49:27 -0700

    docs: sources

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
+/**
+ * Sources used to build an Express application with Typescript and DynamoDB
+ * 1. https://dev.to/wizdomtek/typescript-express-building-robust-apis-with-nodejs-1fln
+ * 2. https://blog.logrocket.com/express-typescript-node/
+ * 3. https://blog.postman.com/how-to-create-a-rest-api-with-node-js-and-express/
+ * 4. https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/programming-with-javascript.html
+ * 5. https://expressjs.com/en/guide/routing.html
+ * 6. https://faerulsalamun.medium.com/restful-api-with-node-js-express-and-dynamodb-5059beb3ba7f
+ * 7. https://tapiwanashekanda.hashnode.dev/how-to-create-a-crud-api-using-expressjs-and-aws-dynamodb
+ */
+
 import dotenv from "dotenv";
 import express from "express";
 
